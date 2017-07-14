71de842cf78412ee12d5cc89bb46c791ef782a0e
André Clinio <clinio@tecgraf.puc-rio.br>
2017-07-14 18:28:57 -0300

    teste

--- src/app/profile/cloud/cloud.component.js
+++ src/app/profile/cloud/cloud.component.js
@@ -1,23 +1,22 @@
 (function () {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('core')
-		.component('ttProfileCloud', {
-			templateUrl: 'app/profile/cloud/cloud.component.html',
-			controller: controller,
-			controllerAs: '$ctrl',
-			bindings: {
-               user: '<'
-            }
-	});
+  angular
+    .module('core')
+    .component('ttProfileCloud', {
+      templateUrl: 'app/profile/cloud/cloud.component.html',
+      controller: controller,
+      controllerAs: '$ctrl',
+      bindings: {
+        user: '<'
+      }
+    });
 
-	/** @ngInject */
-	function controller() {
-		var $ctrl = this;
-		$ctrl.$onInit = function() {
-
-		}
-	}
+  /** @ngInject */
+  function controller() {
+    var $ctrl = this;
+    $ctrl.$onInit = function () {
+    }
+  }
 
 })();
