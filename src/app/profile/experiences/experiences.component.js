9e7bfb7299cad2df29725298b1cade2eb49c563e
André Clinio <clinio@tecgraf.puc-rio.br>
2017-07-17 18:30:17 -0300

    Clinio: Teste com ui-grid

--- src/app/profile/experiences/experiences.component.js
+++ src/app/profile/experiences/experiences.component.js
@@ -13,9 +13,51 @@
     });
 
   /** @ngInject */
-  function controller() {
+  function controller($log) {
     var $ctrl = this;
-    $ctrl.$onInit = function() {}
+
+    $ctrl.mountGridData = function(experiences) {
+      var gData = { 
+        columnDefs: [ 
+          { name: 'Nome', field: 'name'},
+          { name: 'Data', field: 'date'},
+          { name: 'Competências', field: 'skills'},
+          { name: 'Projeto', field: 'project', cellTemplate: '<span class="badge">{{grid.getCellValue(row, col)}}</span>'},
+          { name: '', field: 'delete', cellTemplate: '<button class="fa fa-trash" ng-click="console.log(1)"></button>'}
+        ],
+        data: []
+      };
+
+      for (var i = 0; i < experiences.length; i++) {
+         var exp = experiences[i];
+         gData.data[i] = {
+           name: exp.getDescription(),
+           date: exp.getStartDate() + " - " + exp.getEndDate(),
+           skills: "",
+           project: exp.getProject().getName(),
+           delete: exp
+         };
+
+         var skills = exp.getSkills();
+         var skillsData = "";
+         for (var s = 0; s < skills.length; s++) {
+           var sk = skills[s];
+           var skName = sk.getSkill().getName();
+            skillsData += skName + " ";
+         }
+         gData.data[i].skills = skillsData;
+      }
+      return gData;
+    }
+    
+    $ctrl.delExperience = function(experience) {
+       $log.log("Apagando", experience);
+    }
+
+    $ctrl.$onInit = function() {
+      $ctrl.gridData = $ctrl.mountGridData($ctrl.user.getExperiences());
+      $log.log($ctrl.user.getExperiences());
+    }
   }
 
 
