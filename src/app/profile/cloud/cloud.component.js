afa70c19d1792d66352398b190bb5bd4e8d34495
André Clinio <clinio@tecgraf.puc-rio.br>
2017-07-25 13:08:11 -0300

    Cloud funcionando

--- src/app/profile/cloud/cloud.component.js
+++ src/app/profile/cloud/cloud.component.js
@@ -1,8 +1,8 @@
-(function () {
+(function() {
   'use strict';
 
   angular
-    .module('core')
+    .module('profile')
     .component('ttProfileCloud', {
       templateUrl: 'app/profile/cloud/cloud.component.html',
       controller: controller,
@@ -15,7 +15,21 @@
   /** @ngInject */
   function controller() {
     var $ctrl = this;
-    $ctrl.$onInit = function () {
+    $ctrl.$onInit = function() {
+      updateWords();
+    }
+
+    $ctrl.onDestroy = function() {
+      $('#wordsCloud').jQCloud('destroy');
+    }
+
+    var updateWords = function() {
+      var skills = $ctrl.user.getSkills();
+      $ctrl.words = [];
+      angular.forEach(skills, function(s) {
+        $ctrl.words.push({ text: s.getName(), weight: s.getLevel()});
+      });
+      $('#wordsCloud').jQCloud($ctrl.words, { delay: 10, shape: 'rectangular', height: 300, center: { x: 0.45, y: 0.5} });
     }
   }
 
