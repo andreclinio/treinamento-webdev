6e22f60283ea361e9efc256d887a24ba2005c409
André Clinio <clinio@tecgraf.puc-rio.br>
2017-07-28 18:05:11 -0300

    Clinio: Remodularização para loop com níveis

--- src/app/shared/components/skill-subtitle/skill-subtitle.component.js
+++ src/app/shared/components/skill-subtitle/skill-subtitle.component.js
@@ -13,9 +13,18 @@
     });
 
   /** @ngInject */
-  function controller() {
+  function controller($log, ttModelUtilService, ttGuiUtilService) {
     var $ctrl = this;
     $ctrl.$onInit = function () {
+      $ctrl.title = $ctrl.title || "Legenda para Níveis de Competência";
+      $ctrl.levels = [];
+      for (var i = 0; i < 3; i++) {
+        var obj = {};
+        obj.text = ttModelUtilService.getSkillLevelName(i+1);
+        obj.class = ttGuiUtilService.getTextClassForSkillLevel(i+1);
+        $ctrl.levels.push(obj);
+      } 
+      $log.log($ctrl.levels);
     }
 
     $ctrl.$onDestroy = function () {
