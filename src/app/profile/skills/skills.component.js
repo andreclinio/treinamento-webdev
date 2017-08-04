387f7c99cd55c8e0deed28ce75c0e3355cedda09
Carlos Viana <carlosjmviana@gmail.com>
2017-08-04 16:05:18 -0300

    cviana:  - Ajustando a ação de visualização dos dados da competência. Refactoring no código para podermos utilizar o mesmo HTML em ambos as colunas Experiências/Projetos e Ações.

--- src/app/profile/skills/skills.component.js
+++ src/app/profile/skills/skills.component.js
@@ -36,18 +36,20 @@
       var projCellTemplate = '<span class="badge" ng-click="grid.appScope.showProjectsDetails(grid.getCellValue(row, col).projects)">{{grid.getCellValue(row, col).projects.length}}</span><span>Projetos</span>'
       var expProjectsCellTemplate = expCellTemplate + " " + projCellTemplate;
 
-      var delCellTemplate = '<span class="fa fa-trash" ng-click="grid.appScope.delExperience(grid.getCellValue(row, col))"></span>';
-      var viewCellTemplate = '<span class="fa fa-file-o" ng-click="grid.appScope.showExperienceDetails(grid.getCellValue(row, col))"></span>';
+      var delCellTemplate = '<span class="fa fa-trash tt-fa-widget" ng-click="grid.appScope.delSkill(grid.getCellValue(row, col))" title="Apagar Competência"></span>';
+      var viewCellTemplate = '<span class="fa fa-eye tt-fa-widget" ng-click="grid.appScope.showSkillDetails(grid.getCellValue(row, col))" title="Ver detalhes da Competência"></span>';
       var actionsCellTemplate = delCellTemplate + viewCellTemplate;
+
+      var expProjHeader = 'Experiências / Projetos';
 
       var gridData = {
         enableColumnsResizing: true,
         enableColumnMenus: false,
         columnDefs: [
           { name: 'Competência', field: 'skillName', width: '15%' },
-          { name: 'Nível', field: 'skillLevel', width: '10%', cellTemplate: levelCellTemplate, },
-          { name: 'Experiências / Projetos', field: 'expProjects', width: '70%', cellTemplate: expProjectsCellTemplate, enableSorting: false },
-          { name: 'Ações', field: 'actions', width: '5%', cellTemplate: actionsCellTemplate, enableSorting: false, enableColumnResizing: false, enableHiding: false }
+          { name: 'Nível',       field: 'skillLevel', width: '10%', cellTemplate: levelCellTemplate, },
+          { name: expProjHeader, field: 'skillLine', width: '65%', cellTemplate: expProjectsCellTemplate, enableSorting: false },
+          { name: 'Ações',       field: 'skillLine', width: '10%', cellTemplate: actionsCellTemplate, enableSorting: false, enableColumnResizing: false, enableHiding: false }
         ],
         data: []
       };
@@ -57,19 +59,17 @@
         var skill = skills[i];
         var userExperiences = getExperiencesBySkillId(skill.getId());
         var userExpProjects = getProjectsByExperiences(userExperiences);
-        // $log.log("Skill["+i+"]: " + skill.getId());
-        // $log.log(userExperiences);
-        var objects = {
-          experiences: userExperiences,
-          projects: userExpProjects
-        };
         gridData.data[i] = {
           skillName: skill.getName(),
           skillLevel: skill.getLevel(),
           expCount: skill.getExperienceCount(),
           projCount: skill.getProjectCount(),
           projectSkills: "(" + skill.getExperienceCount() + ") Experiências " + " (" + skill.getProjectCount() + ") Projetos",
-          expProjects: objects
+          skillLine: {
+            skill: skill,
+            experiences: userExperiences,
+            projects: userExpProjects
+          }
         };
       }
       return gridData;
@@ -106,38 +106,41 @@
     }
 
     $scope.showExperiencesDetails = function (experiences) {
-      var html = '<div>';
-      if (experiences.length == 0) {
-        html += '<h2>Não existem experiências associadas</h2>';
-      }
-      else {
-        experiences.forEach(function (e) {
-          html += '<h2>Título: ' + e.getTitle() + '</h2>';
-          html += '<h3>Início: ' + e.getStartDate() + '</h3>';
-          html += '<h3>Término: ' + e.getEndDate() + '</h3>';
-          html += '</br>'
-        }, this);
-      }
-      html += '</div>';
-
+      var html = getExperiencesHtml(experiences);
       ttGuiUtilService.showMessage("Experiência de " + $ctrl.user.getName(), html);
     }
 
     $scope.showProjectsDetails = function (projects) {
-      // $log.log("projects: ", projects);
-      var html = '<div>';
-      if (projects.length == 0) {
-        html += '<h2>Não existem projetos associados</h2>';
-      }
-      else {
-        projects.forEach(function (p) {
-          html += '<h2>Nome: ' + p.getName() + '</h2>';
-          html += '<h3>Descrição: ' + (p.getDescription() != undefined ? p.getDescription() : "Não informada") + '</h3>';
-          html += '</br>'
-        }, this);
-      }
-      html += '</div>';
+      var html = getProjectsHtml(projects);
       ttGuiUtilService.showMessage("Projeto de " + $ctrl.user.getName(), html);
+    }
+
+    $scope.delSkill = function (skill) {
+      // var title = experience.getTitle();
+      // var warnMsg = "Deseja realmente apagar a experiência '" + title + "'?";
+      // var promisse = ttGuiUtilService.confirmWarningMessage(null, warnMsg, "Apagar");
+      // promisse.then(function () {
+      //   $ctrl.user.removeExperience(experience);
+      //   userDataService.update($ctrl.user);
+      //   var infoMsg = "Experiência '" + title + "'apagada com sucesso."
+      //   ttGuiUtilService.showInfoMessage(null, infoMsg);
+      // }, function () {
+      //   $log.log('operação cancelada');
+      // });
+    }
+
+    $scope.showSkillDetails = function (skillLine) {
+      var skill = skillLine.skill;
+      var desc = skill.getDescription() || "(sem texto disponível)";
+      var html = '<h2>' + skill.getName()  + '</h2>' +
+                 '<h3>' + $scope.getSkillLevelText(skill.getLevel()) + '</h3>' +
+                 '<p>' + desc + '</p>';
+      var htmlExps = getExperiencesHtml(skillLine.experiences);
+      var htmlProjs = getProjectsHtml(skillLine.projects);
+      html += '<h2>Experiências</h2>' + htmlExps;
+      html += '</br>';
+      html += '<h2>Projetos</h2>' + htmlProjs;
+      ttGuiUtilService.showMessage("Competência de " + $ctrl.user.getName(), html);
     }
 
     $ctrl.addSkillUserCallback = function (data, skillUser) {
@@ -156,6 +159,39 @@
         callback: $ctrl.addSkillUserCallback
       };
       ttGuiUtilService.runFormOperation($rootScope, formName, data, title, html, "Criar", null, "md");
+    }
+
+    function getExperiencesHtml (experiences) {
+      var html = '<div>';
+      if (experiences.length == 0) {
+        html += '<h3>Não existem experiências associadas</h3>';
+      }
+      else {
+        experiences.forEach(function (e) {
+          html += '<h3>Título: ' + e.getTitle() + '</h3>';
+          html += '<h4>Início: ' + e.getStartDate() + '</h4>';
+          html += '<h4>Término: ' + e.getEndDate() + '</h4>';
+        }, this);
+      }
+      html += '</div>';
+
+      return html;
+    }
+
+    function getProjectsHtml (projects) {
+      var html = '<div>';
+      if (projects.length == 0) {
+        html += '<h3>Não existem projetos associados</h3>';
+      }
+      else {
+        projects.forEach(function (p) {
+          html += '<h3>Nome: ' + p.getName() + '</h3>';
+          html += '<h4>Descrição: ' + (p.getDescription() != undefined ? p.getDescription() : "Não informada") + '</h4>';
+        }, this);
+      }
+      html += '</div>';
+
+      return html;
     }
 
   }
